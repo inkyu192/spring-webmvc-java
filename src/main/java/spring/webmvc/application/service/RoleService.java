39b4cfe6e3e1ead1e540ab75d681373ec698f9d2
inkyu192 <inkyu192@gmail.com>
2025-04-28 18:39:37 +0900

    중복 코드 제거

--- src/main/java/spring/webmvc/application/service/RoleService.java
+++ src/main/java/spring/webmvc/application/service/RoleService.java
@@ -1,18 +1,13 @@
 package spring.webmvc.application.service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import spring.webmvc.domain.model.entity.Permission;
 import spring.webmvc.domain.model.entity.Role;
-import spring.webmvc.domain.repository.PermissionRepository;
 import spring.webmvc.domain.repository.RoleRepository;
-import spring.webmvc.presentation.exception.EntityNotFoundException;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,22 +15,13 @@ import spring.webmvc.presentation.exception.EntityNotFoundException;
 public class RoleService {
 
 	private final RoleRepository roleRepository;
-	private final PermissionRepository permissionRepository;
+	private final PermissionService permissionService;
 
 	@Transactional
 	public Role createRole(String name, List<Long> permissionIds) {
 		Role role = Role.create(name);
 
-		Map<Long, Permission> permissionMap = permissionRepository.findAllById(permissionIds).stream()
-			.collect(Collectors.toMap(Permission::getId, permission -> permission));
-
-		for (Long id : permissionIds) {
-			Permission permission = permissionMap.get(id);
-			if (permission == null) {
-				throw new EntityNotFoundException(Permission.class, id);
-			}
-			role.addPermission(permission);
-		}
+		permissionService.addPermission(permissionIds, role::addPermission);
 
 		return roleRepository.save(role);
 	}
