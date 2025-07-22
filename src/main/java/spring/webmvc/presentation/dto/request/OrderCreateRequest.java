885e68dcf803ce50c7624117c0de958eb77f74e9
inkyu192 <inkyu192@gmail.com>
2025-07-22 11:19:32 +0900

    command request 의존성 제거

--- src/main/java/spring/webmvc/presentation/dto/request/OrderCreateRequest.java
+++ src/main/java/spring/webmvc/presentation/dto/request/OrderCreateRequest.java
@@ -3,6 +3,8 @@ package spring.webmvc.presentation.dto.request;
 import java.util.List;
 
 import jakarta.validation.constraints.Size;
+import spring.webmvc.application.dto.command.OrderCreateCommand;
+import spring.webmvc.application.dto.command.OrderProductCreateCommand;
 
 public record OrderCreateRequest(
 	@Size(min = 1)
@@ -12,5 +14,13 @@ public record OrderCreateRequest(
 		if (products == null) {
 			products = List.of();
 		}
+	}
+
+	public OrderCreateCommand toCommand() {
+		return new OrderCreateCommand(
+			products.stream()
+				.map(it -> new OrderProductCreateCommand(it.id(), it.quantity()))
+				.toList()
+		);
 	}
 }
